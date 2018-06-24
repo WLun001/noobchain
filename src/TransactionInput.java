7a7ebd590c2b6e3810b04f7f49facb8afd3d436c
Wei Lun <weilun_95@hotmail.com>
2018-06-24 15:02:40 +0800

    added transactionintput/output

--- src/TransactionInput.java
+++ src/TransactionInput.java
@@ -1,2 +1,17 @@
 public class TransactionInput {
+    private String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+    private TransactionOutput UTXO; //container Unspent Transaction Output
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+
+    public String getTransactionOutputId() {
+        return transactionOutputId;
+    }
+
+    public TransactionOutput getUTXO() {
+        return UTXO;
+    }
+
 }
--- src/TransactionOutput.java
+++ src/TransactionOutput.java
@@ -1,2 +1,38 @@
+import java.security.PublicKey;
+
 public class TransactionOutput {
+    private String id;
+    private PublicKey recipient; // also known as the new owner of these coins
+    private float value; // the amount of the coins they own
+    private String parentTransactionId; // the id of the transaction this output was created in
+
+    public TransactionOutput(PublicKey recipient, float value, String parentTransactionId) {
+        this.recipient = recipient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256(
+                StringUtil.getStringFromKey(recipient) +
+                        Float.toString(value) +
+                        parentTransactionId);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public PublicKey getRecipient() {
+        return recipient;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public String getParentTransactionId() {
+        return parentTransactionId;
+    }
+
+    public boolean isMine(PublicKey publicKey) {
+        return publicKey == recipient;
+    }
 }
